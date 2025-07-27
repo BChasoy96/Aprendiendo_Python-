b94881bc87f2e5cba6a0cf8c053976ad622d87eb
Brayan Alexander Chasoy <168701834+BChasoy96@users.noreply.github.com>
2025-07-27 14:31:52 -0500

    Update Cinematica_2R.py

--- Cinematica_2R.py
+++ Cinematica_2R.py
@@ -1,1 +1,26 @@
-import numpy as np
+from roboticstoolbox import *
+from spatialmath.base import *
+import math
+
+h1 = 0
+h2 = 0
+l1 = 4
+l2 = 4
+
+q1 = 0
+q2 = 0
+
+R = []
+R.append(RevoluteDH(d=0, alpha=0, a=l1, offset=0))
+R.append(RevoluteDH(d=0, alpha=0, a=l2, offset=0))
+
+Robot = DHRobot(R, name='Bender')
+print(Robot)
+
+Robot.teach([q1, q2], 'rpy/zyx', limits=[-30,30,-30,30,-30,30])
+
+#zlim([-15,30]);
+
+MTH = Robot.fkine([q1,q2])
+print(MTH)
+print(f'Roll, Pitch, Yaw = {tr2rpy(MTH.R, 'deg', 'zyx')}')
